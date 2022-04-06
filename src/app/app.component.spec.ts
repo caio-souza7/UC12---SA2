cc1742f259d2bc372fe45bc7d11bf80f9e654523
caio-souza7 <caio.student7@gmail.com>
2022-04-06 20:19:34 -0300

    Subindo Projeto Angular LH Vagas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'LHAngular-Front'`, () => {
+  it(`should have as title 'lh-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('LHAngular-Front');
+    expect(app.title).toEqual('lh-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('LHAngular-Front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('lh-angular app is running!');
   });
 });
